0a4baabb49d5675109ff5e87e85816ef5afe2537
samyak989 <samyak989@gmail.com>
2021-05-13 21:45:30 +0530

    added the action buttons
    action buttons like update and delete

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 app = Flask(__name__)
@@ -14,19 +14,37 @@ class Todo(db.Model):
 def __repr__(self) -> str:
     return f"{self.sno} - {self.title}"
 
-@app.route('/')
+@app.route('/', methods = ["GET", "POST"])
 def hello_world():
-    todo = Todo(title = "First Todo", desc = "My first task")
-    db.session.add(todo)
-    db.session.commit()
+    if request.method == 'POST':
+        title = request.form["title"]
+        desc = request.form["desc"]
+        todo = Todo(title = title, desc = desc)
+        db.session.add(todo)
+        db.session.commit()
     allTodo = Todo.query.all()
     return render_template("index.html", allTodo = allTodo)
 
-@app.route('/show')
-def products():
-    allTodo = Todo.query.all()
-    print(allTodo)
-    return 'Here are some products'
+@app.route('/update/<int:sno>', methods = ["GET", "POST"])
+def update(sno):
+    if request.method == 'POST':
+        title = request.form["title"]
+        desc = request.form["desc"]
+        todo = Todo.query.filter_by(sno = sno).first()
+        todo.title = title
+        todo.desc = desc
+        db.session.add(todo)
+        db.session.commit()
+        return redirect("/")
+    todo = Todo.query.filter_by(sno = sno).first()
+    return render_template("update.html", todo = todo)
+
+@app.route('/delete/<int:sno>')
+def delete(sno):
+    todo = Todo.query.filter_by(sno = sno).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect("/")
 
 if __name__ == '__main__':
     app.run(debug=True) 
